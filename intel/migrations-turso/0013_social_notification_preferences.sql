cde90cd4b83d7c4b9615e3ff829e68c736dfd3e5
Titus Paine <256450069+tituspaine@users.noreply.github.com>
2026-09-18 16:13:32 -0400

    intel migrations: correct 0013 recorded version

--- intel/migrations-turso/0013_social_notification_preferences.sql
+++ intel/migrations-turso/0013_social_notification_preferences.sql
@@ -14,4 +14,4 @@ CREATE INDEX idx_notifications_user_unread_time
 ON notifications(user_id,read_at,created_at DESC,id DESC);
 
 INSERT OR IGNORE INTO schema_migrations(version,applied_at)
-VALUES('0013_social_social_notification_preferences',unixepoch()*1000);
+VALUES('0013_social_notification_preferences',unixepoch()*1000);
